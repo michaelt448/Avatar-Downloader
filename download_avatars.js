5193714a3e35baa492d1426aa35726dc994d328d
vagrant <mikegithub77@gmail.com>
2019-05-07 00:36:49 +0000

    added a token to the repo to have larger access to private

--- download_avatars.js
+++ download_avatars.js
@@ -3,7 +3,15 @@ var request = require('request');
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 const getRepoContributors = function(repoOwner, repoName, cb) {
-
+  var options = {
+   url : `https://api.github.com/repos/${repoOwner}/${repoName}/contributors`,
+   headers: {
+    'User-Agent' : 'request'
+   }
+};
+  request(options,function(err, res, body) {
+    cb(err,body);
+  });
 }
 
 
