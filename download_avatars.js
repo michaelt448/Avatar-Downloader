e53cba24fd5921dc298a9425ebe19ec73ae408a8
vagrant <mikegithub77@gmail.com>
2019-05-07 00:50:29 +0000

    parsed body and changed call back function to print the login and the avatar for each person

--- download_avatars.js
+++ download_avatars.js
@@ -1,21 +1,25 @@
 var request = require('request');
-
+var token = require('./secret');
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 const getRepoContributors = function(repoOwner, repoName, cb) {
   var options = {
    url : `https://api.github.com/repos/${repoOwner}/${repoName}/contributors`,
    headers: {
-    'User-Agent' : 'request'
+    'User-Agent' : 'request',
+    Authorization : 'token ' + token.GITHUB_TOKEN
    }
 };
   request(options,function(err, res, body) {
-    cb(err,body);
+    var data = JSON.parse(body);
+    cb(err,data);
   });
 }
 
 
 getRepoContributors("jquery", "jquery", function(err,result) {
   console.log("Erros:", err);
-  console.log("Result: ", result);
-})
+  result.forEach(function(person) {
+    console.log(person.login + ' is ' + person.avatar_url);
+  });
+});
