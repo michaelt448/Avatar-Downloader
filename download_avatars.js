27702e78b5fe4d33be1aee803f9019b049a322ad
vagrant <mikegithub77@gmail.com>
2019-05-07 00:16:33 +0000

    made a stub for function which gets contributors and simple call back functions which will print errors or results

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,13 @@
 var request = require('request');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
+
+const getRepoContributors = function(repoOwner, repoName, cb) {
+
+}
+
+
+getRepoContributors("jquery", "jquery", function(err,result) {
+  console.log("Erros:", err);
+  console.log("Result: ", result);
+})
